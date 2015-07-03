30d85b2dd709d06939c1baae39160d0fe901e6f4
Artem Dudecar <e4e5f4@yahoo.com>
2015-07-03 04:52:43 +0300

    Добавил валидатор длинны.

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,8 @@
 from flask.ext.wtf import Form
 from wtforms import TextField, BooleanField, PasswordField
-from wtforms.validators import Required
+from wtforms.validators import Required, Length
 
 class LoginForm(Form):
-	nick = TextField('nick', validators = [Required()])
-	password = PasswordField('password', validators = [Required()])
+	nick = TextField('nick', validators = [Required(), Length(min = 1, max = 21, message = "Too long")])
+	password = PasswordField('password', validators = [Required(), Length(min = 1, max = 21, message = "Too long")])
 	remember_me = BooleanField('remember_me', default = False)
