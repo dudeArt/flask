2f1902d99dbec3f6612cfb07563b1e990aff91f6
Artem Dudecar <e4e5f4@yahoo.com>
2015-07-03 04:39:32 +0300

    Запилил авторизацию и регу (свою(быдлокод)). Правда не могу понять почему не отображаются флеши...

--- app/views.py
+++ app/views.py
@@ -1,11 +1,6 @@
-from app import app
+from app import app, db, models
 from flask import render_template, flash, redirect
 from forms import LoginForm
-
-users = [
-	{'nick': 'Dude', 'name': 'Dudka Artem Vadimovich'},
-	{'nick': 'max', 'name': 'Piqulin Maxim Romanovich'}
-]
 
 @app.route('/')
 @app.route('/index')
@@ -13,13 +8,9 @@ def index():
 	return render_template('index.html',
 					title = 'Main Page')
 
-@app.route('/profile/<nick>')
-def profile(nick):
-	user = None
-	for el in users:
-		if el['nick'] == nick:
-			user = el
-			break
+@app.route('/profile/<int:id>')
+def profile(id):
+	user = models.User.query.filter_by(id = id).first()
 	return render_template('profile.html',
 						title = 'Profile',
 						user = user)
@@ -29,8 +20,28 @@ def profile(nick):
 def login():
 	form = LoginForm()
 	if form.validate_on_submit():
-		return redirect('/profile/' + str(form.nick.data))
+		user = models.User.query.filter_by(nick = str(form.nick.data), password = str(form.password.data)).first()
+		if user:
+			flash('User ' + str(user.nick) + ' has successfully logged in.')
+			return redirect('/profile/' + str(user.id))
 	flash('Data has been input incorrectly.')
 	return render_template('login.html',
 							title = 'Log In',
 							form = form)
+
+@app.route('/reg', methods = ['GET', 'POST'])
+def reg():
+	form = LoginForm()
+	if form.validate_on_submit():
+		user = models.User(nick = unicode(str(form.nick.data), "utf-8"), password = unicode(str(form.password.data), "utf-8"))
+		db.session.add(user)
+		if db.session.commit():
+			flash("User " + str(user.nick) + " has benn successfully added.")
+		else:
+			flash("Error while adding user " + str(user.nick))
+		return redirect('/')
+	return render_template('reg.html',
+						title = 'Registration',
+						form = form)
+
+
